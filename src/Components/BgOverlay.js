56d46e323bfc0fe87e676786a1969e90080acbc7
Hoi Wong <jameshf420@gmail.com>
2023-03-17 09:41:03 -0400

    remove related css for onoff button

--- src/Components/BgOverlay.js
+++ src/Components/BgOverlay.js
@@ -1,11 +1,8 @@
 //imported Hooks
 import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-
-
 const BgOverlay = () => {
    const [position, setPosition] = useState({ x: 0, y: 0 });
-   const [isSpinning, setSpinning] = useState(true);
+   
    useEffect(() => {
       const updatePosition = e => {
         setTimeout(() => {
@@ -17,7 +14,7 @@ const BgOverlay = () => {
     }, []);
    return (
         <>
-          <div className={isSpinning ? 'isSpinning' : ''} 
+          <div className='isSpinning'
           style={{ position: 'fixed', left: position.x, top: position.y }}
           >
           </div>
